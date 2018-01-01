fde0c0f55fff1b7425d8df138343fb8a0115d946
voakgh <victorthinkahead@gmail.com>
2018-01-01 11:42:20 +0000

    accepted the merge and corrected few lines in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
-<<<<<<< HEAD
-=======
 
->>>>>>> master
+
+
+
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -16,12 +16,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
-<<<<<<< HEAD
-=======
-    
->>>>>>> master
     HomePage,
-    TabsPage
+    TabsPage,
   ],
   imports: [
     BrowserModule,
@@ -31,12 +27,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-<<<<<<< HEAD
-=======
-    
->>>>>>> master
     HomePage,
-    TabsPage
+    TabsPage,
   ],
   providers: [
     StatusBar,
