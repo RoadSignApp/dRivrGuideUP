38cf38999901b1cc99b0116e6f1f658666a74f92
voakgh <victorthinkahead@gmail.com>
2018-01-04 09:56:29 +0000

    have made some corrections

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,7 @@
-import { Geolocation } from '@ionic-native/geolocation';
+
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 
 
