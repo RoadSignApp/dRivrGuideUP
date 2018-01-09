9a14960d029b26f59d2652451d4896d99d4d7641
voakgh <victorthinkahead@gmail.com>
2018-01-09 09:32:18 +0000

    working on the map

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,5 @@
-
-import { Component } from '@angular/core';
+//import { GoogleMaps } from '@ionic-native/google-maps';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 
@@ -11,74 +11,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
-import {
-  GoogleMaps,
-  GoogleMap,
-  GoogleMapsEvent,
-  GoogleMapOptions,
-  CameraPosition,
-  MarkerOptions,
-  Marker,
+declare var google;
 
 
-} from '@ionic-native/google-maps';
 
-
-@IonicPage()
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html',
 })
 export class MapPage {
-  map: GoogleMap;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private googleMaps: GoogleMaps) {
+  @ViewChild('map') mapElement;
+  map: any;
+
+
+  
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-  ionViewDidLoad() {
-    this.loadMap();
+  ionViewDidLoad(){
+    this.initMap();
   }
+      initMap(){
+        let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+        let mapOptions = {
+           center: latLng,
+           zoom: 15,
+           mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
-  loadMap() {
 
-    let mapOptions: GoogleMapOptions = {
-      camera: {
-        target: {
-          lat: 43.0741904,
-          lng: -89.3809802
-        },
-        zoom: 18,
-        tilt: 30
       }
-    };
-
-    this.map = this.googleMaps.create('map_canvas', mapOptions);
-
-    // Wait the MAP_READY before using any methods.
-    this.map.one(GoogleMapsEvent.MAP_READY)
-      .then(() => {
-        console.log('Map is ready!');
-
-        // Now you can use all methods safely.
-        this.map.addMarker({
-          title: 'Ionic',
-          icon: 'blue',
-          animation: 'DROP',
-          position: {
-            lat: 43.0741904,
-            lng: -89.3809802
-          }
-        })
-          .then(marker => {
-            marker.on(GoogleMapsEvent.MARKER_CLICK)
-              .subscribe(() => {
-                alert('clicked');
-              });
-          });
-
-      });
-  }
-
-
-
+  
 }
--- src/service-worker.js
+++ src/service-worker.js
@@ -33,9 +33,9 @@ self.toolbox.router.default = self.toolbox.networkFirst;
 angular.module('ionicApp', ['ionic'])
 
 .controller('MyCtrl', function($scope) {
-  $scope.myTitle = 'Template';
-  
-  $scope.doSomething = function() {
-    $scope.myTitle = $scope.myTitle + ' something';
-  };
+    $scope.myTitle = 'Template';
+
+    $scope.doSomething = function() {
+        $scope.myTitle = $scope.myTitle + ' something';
+    };
 });
