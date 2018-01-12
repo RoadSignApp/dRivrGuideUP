49bca512e3521a0fd498d6f6e0c3ae2a6b361be4
voakgh <victorthinkahead@gmail.com>
2018-01-12 15:35:02 +0000

    home page update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -8,8 +10,18 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
 
   }
+   showLongToast(position: string) {
+    let toast = this.toastCtrl.create({
+      message: 'I have always been attracted to cars, and driving is a completely measurable experience: if you qualify last on the grid, you are the slowest, and if you qualify first on the grid, you are the fastest. So no one can say you are slow if you are fast and no one can say you are fast if you are slow.',
+     showCloseButton: true,
+      closeButtonText: 'Ok'
+    });
+    toast.present();
+  }
+
+   
 
 }
