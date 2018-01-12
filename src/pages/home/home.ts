d56d79d80948e188e714855e45ae591ad27b2430
voakgh <victorthinkahead@gmail.com>
2018-01-12 15:11:47 +0000

    home page update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  tab2Root = AboutPage;
 
 
   constructor(public navCtrl: NavController) {
